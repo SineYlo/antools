abee9e83ff4b8edb95724270c585882e6f0f8ced
SineYlo <vavshhik01@mail.ru>
2022-01-30 19:27:10 +1000

    feat: all project files have been added

---
+++ gulp/tasks/image-optimization.js
@@ -0,0 +1,39 @@
+/* eslint-disable arrow-body-style */
+// |=============== CONNECTING ALL MODULES ===============>
+import { src, dest } from 'gulp';
+import squoosh from 'gulp-libsquoosh';
+import { path } from '../config';
+
+// |=============== SETTING UP AN IMAGE OPTIMIZATION AND CONVERSION TASK - JPG ===============>
+const imageOptimizationJpg = () => {
+  return src(path.source.convertjpg)
+    .pipe(squoosh({
+      mozjpeg: {},
+      webp: {},
+      avif: {},
+    }))
+    .pipe(dest(path.build.pictures));
+};
+
+// |=============== SETTING UP AN IMAGE OPTIMIZATION AND CONVERSION TASK - PNG ===============>
+const imageOptimizationPng = () => {
+  return src(path.source.convertpng)
+    .pipe(squoosh({
+      oxipng: {},
+      webp: {},
+      avif: {},
+    }))
+    .pipe(dest(path.build.pictures));
+};
+
+// |=============== SETTING UP AN IMAGE OPTIMIZATION AND CONVERSION TASK - FAVICONS ===============>
+const imageOptimizationFav = () => {
+  return src(path.source.convertfav)
+    .pipe(squoosh({
+      oxipng: {},
+    }))
+    .pipe(dest(path.build.pictures));
+};
+
+// |=============== EXPORTING THE MAIN VARIABLE FOR USE ===============>
+export { imageOptimizationJpg, imageOptimizationPng, imageOptimizationFav };
