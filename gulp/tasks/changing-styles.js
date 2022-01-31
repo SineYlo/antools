a54cb8f860bb81405d425c282b5a1f3e618668e4
SineYlo <vavshhik01@mail.ru>
2022-01-31 20:33:38 +1000

    feat: added some exceptions to gulp tasks

--- gulp/tasks/changing-styles.js
+++ gulp/tasks/changing-styles.js
@@ -42,7 +42,7 @@ const changingStyles = () => {
       postcssPxToRem({
         rootValue: 16,
         unitPrecision: 5,
-        propList: ['*', '!border*', '!outline*'],
+        propList: ['*', '!border*', '!outline*', '!*-shadow'],
         selectorBlackList: ['html'],
         replace: true,
         mediaQuery: false,
