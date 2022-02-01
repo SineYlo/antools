28a9f01fe02540e3f08b5df9e355f96f5c6a5954
SineYlo <vavshhik01@mail.ru>
2022-02-02 00:28:20 +1000

    feat: markup has been written for all sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 // |=============== CONNECTING EXTERNAL MODULES ===============>
-// eslint-disable-next-line no-unused-vars
 import focusVisible from 'focus-visible';
 
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
+// |=============== SCRIPT CONNECTION | SLIDER  ===============>
+
+// |=============== SCRIPT CONNECTION | PLACEHOLDER  ===============>
+
+// |=============== SCRIPT CONNECTION | LOADER  ===============>
